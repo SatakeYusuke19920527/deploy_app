f71ab8896aaa91cbd6ff0968aed8657c71309ef8
SatakeYusuke19920527 <sataaaaak@gmail.com>
2020-09-08 08:32:54 +0900

    login logout 完成

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import firebase from '../config/firebase';
+
 const Main = () => {
-	const history = useHistory()
-	
+	const history = useHistory();
 	const handleClick = () => {
-		console.log('success login');
-		history.push('/');
+		firebase
+			.auth()
+			.signOut()
+			.then(function() {
+				// Sign-out successful.
+				console.log('logout');
+				history.push('/');
+			})
+			.catch(function(error) {
+				// An error happened.
+				console.log(error);
+			});
 	};
 	return (
 		<div>
 			<h1>Main</h1>
-			<Button variant="outlined" onClick={handleClick}>戻る</Button>
+			<Button variant="outlined" onClick={handleClick}>
+				ログアウト
+			</Button>
 		</div>
 	);
 };
